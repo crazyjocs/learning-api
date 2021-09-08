410119fa72d14ca25b12303dc7bc22d7c1c2ef4f
crazy_jocs <crazyjocs@gmail.com>
2021-09-08 19:27:01 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,49 +1,71 @@
-import React, { useState, useEffect } from 'react'
-import './App.css'
-import axios from 'axios'
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import useScript from './hooks/useScript';
+//import axios from 'axios';
 
+const api_key = "aa1fJ6CZt7LgUGbp77B56cNW61378cdd";
+const api_url ="https://muse.ai/api/files/videos";
 
 function App() {
 
-  const api_key = "aa1fJ6CZt7LgUGbp77B56cNW61378cdd"
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
 
-  const [videos, setVideos] = useState({})
+  useScript('https://muse.ai/static/js/embed-player.min.js');
 
+  // Note: the empty deps array [] means
+  // this useEffect will run once
+  // similar to componentDidMount()
   useEffect(() => {
-
-    async function fetchData() {
-      const resp = await axios.get('https://muse.ai/api/files/videos', {
-        headers: {
-          'Key': `${api_key}`
+    fetch("https://muse.ai/api/files/videos", {
+      headers : {
+        'Key' : `${api_key}`
+      }
+    })
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          console.log(result);
+          setItems(result);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
         }
-      }).catch(err => err)
-      resp.data.forEach((element, index) => {
-        const tempData = [  element.filename,  element.url]
-        setVideos(oldArray => [...oldArray, tempData])
-      });
-        console.log(videos);
-      //setVideos(oldArray => [...oldArray, newVideo]);
-    }
+      )
 
-    fetchData()
-  }, []);
+  }, [])
 
-  const listVideoDetails = videos.map((video) =>
-    <li>{video}</li>
-  );
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <ul>
 
-  return (
-    <div className="App">
-      <ul>{listVideoDetails}</ul>
-      {/* <video
-        id="video"
-        src={videos}
-        controlsList="nodownload"
-        playsInline={true}
-        autoPlay={true}
-      ></video> */}
-    </div>
-  );
+        {items && !!items.length && items.map(item => (
+          <li key={item.svid}>
+            <h2>Title: <span>{item.title}</span></h2>
+            <div className="videoElement">
+              <div className="muse-video-player"
+                data-video={item.svid}
+                data-links="0"
+                data-logo="0"
+                data-sizing="fill"
+                data-style="minimal">
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    );
+  }
 }
 
 export default App;
