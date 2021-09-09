ecd076db8000d8f731e3b18438acde67d4935c06
crazy_jocs <crazyjocs@gmail.com>
2021-09-09 20:23:15 +0100

    Initial push

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import useScript from './hooks/useScript';
+import React, { useState, useEffect } from 'react'
+import './App.css'
+//import useScript from './hooks/useScript'
+import ReactPlayer from 'react-player'
 //import axios from 'axios';
 
-const api_key = "aa1fJ6CZt7LgUGbp77B56cNW61378cdd";
-const api_url ="https://muse.ai/api/files/videos";
+const api_key = "aa1fJ6CZt7LgUGbp77B56cNW61378cdd"
+const api_url ="https://muse.ai/api/files/videos"
 
 function App() {
 
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [items, setItems] = useState([]);
-
-  useScript('https://muse.ai/static/js/embed-player.min.js');
+  const [error, setError] = useState(null)
+  const [isLoaded, setIsLoaded] = useState(false)
+  const [items, setItems] = useState([])
 
   // Note: the empty deps array [] means
   // this useEffect will run once
@@ -26,40 +25,39 @@ function App() {
       .then(res => res.json())
       .then(
         (result) => {
-          setIsLoaded(true);
-          console.log(result);
-          setItems(result);
+          setIsLoaded(true)
+          console.log(result)
+          setItems(result)
         },
         // Note: it's important to handle errors here
         // instead of a catch() block so that we don't swallow
         // exceptions from actual bugs in components.
         (error) => {
-          setIsLoaded(true);
-          setError(error);
+          setIsLoaded(true)
+          setError(error)
         }
       )
 
   }, [])
 
   if (error) {
-    return <div>Error: {error.message}</div>;
+    return <div>Error: {error.message}</div>
   } else if (!isLoaded) {
-    return <div>Loading...</div>;
+    return <div>Loading...</div>
   } else {
     return (
       <ul>
-
         {items && !!items.length && items.map(item => (
           <li key={item.svid}>
             <h2>Title: <span>{item.title}</span></h2>
-            <div className="videoElement">
-              <div className="muse-video-player"
-                data-video={item.svid}
-                data-links="0"
-                data-logo="0"
-                data-sizing="fill"
-                data-style="minimal">
-              </div>
+            <div className='player-wrapper'>
+              <ReactPlayer
+                className='react-player'
+                url={item.url}
+                width='100%'
+                height='100%'
+                controls='true'
+              />
             </div>
           </li>
         ))}
